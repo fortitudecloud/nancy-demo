5ccf444e0419029111876a3312dcc92cf0b12c10
lionel <lionel.hickey@gmail.com>
2015-07-02 14:09:25 +1000

    nancy-demo

---
+++ src/HelloModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nancy.Demo.Hosting.Docker
+{
+    public class HelloModule : NancyModule
+    {
+
+        public HelloModule()
+            : base("Hello")
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+
+    }
+}
